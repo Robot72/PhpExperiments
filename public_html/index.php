b5a3510e226a83574b3c5395b2164bbabddd89bf
Robert Kuznetsou <robertkuznetsou@gmail.com>
2019-02-26 17:44:21 +0300

    2/7: Released if routing. Added SapiEmitter, Diactoros.

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,39 @@
 <?php
 
-use Framework\Http\Request;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
-$request = new Request();
-echo'<pre>'; print_r($request->getCookieParams());die;
+// Initialization
 
-$name = $_GET['name'] ?? 'Guest';
+$request = ServerRequestFactory::fromGlobals();
 
-header('X-Developer: Robert Kuznetsov');
+// Action
+$path = $request->getUri()->getPath();
 
-echo 'hello ' . $name;
+if ($path === '/') {
+    $name = $request->getQueryParams()['name'] ?? 'Guest';
+    $response = new HtmlResponse('Hello ' . $name . '!');
+} else if ($path === '/about') {
+    $response = new HtmlResponse('I am developer :)');
+} else if (preg_match('#^/blog/(?P<id>\d+)$#i', $path, $matches)) {
+    $id = $matches['id'];
+    if ($id > 2) {
+        $response = new JsonResponse(['error' => 'Undefined page'], 404);
+    } else {
+        $response = new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+    }
+} else {
+    $response = new JsonResponse(['error' => 'Unfound page'], 404);
+}
+
+// Postprocessing
+$response->withHeader('X-Developer', 'Robert Kuznetsov');
+
+// Sending
+
+(new SapiEmitter())->emit($response);
