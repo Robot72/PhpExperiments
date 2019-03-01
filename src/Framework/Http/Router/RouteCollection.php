15a578e98a9e9b2b50a01629701d45c238396162
Robert Kuznetsou <robertkuznetsou@gmail.com>
2019-03-01 19:15:03 +0300

    Releasing router, result, and routecollection.

--- src/Framework/Http/Router/RouteCollection.php
+++ src/Framework/Http/Router/RouteCollection.php
@@ -13,4 +13,19 @@ class RouteCollection
         $this->routes = new Route($name, $pattern, $handler, ['GET'], $tokens);
     }
 
+    public function post($name, $pattern, $handler, array $tokens = [])
+    {
+        $this->routes = new Route($name, $pattern, $handler, ['POST'], $tokens);
+    }
+
+    public function any($name, $pattern, $handler, array $tokens = [])
+    {
+        $this->routes = new Route($name, $pattern, $handler, [], $tokens);
+    }
+
+    public function getRouter()
+    {
+        return $this->routes;
+    }
+
 }
--- src/Framework/Http/Router/Router.php
+++ src/Framework/Http/Router/Router.php
@@ -4,7 +4,19 @@
 namespace Framework\Http\Router;
 
 
+use Psr\Http\Message\ServerRequestInterface;
+
 class Router
 {
+    private $routes;
 
+    public function __construct(RouteCollection $collection)
+    {
+        $this->routes = $collection;
+    }
+
+    public function match(ServerRequestInterface $request)
+    {
+        return new Result($name, $handler, $attributes);
+    }
 }
